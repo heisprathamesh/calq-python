b92e50a29de877ec0505b0c35d37f3c6129f3dab
Prathamesh karve <prathameshkarve19@gmail.com>
2023-07-19 17:13:56 +0530

    Add files via upload

---
+++ calculator python.py
@@ -0,0 +1,17 @@
+in1 = int(input("Enter First Number "))
+op= input("Enter Your Operator")
+in2= int(input("Enter Your Sec Number"))
+
+if op == "+":
+    print(in1 + in2)
+
+elif op == "-":
+    print(in1 + in2)
+elif op == "*":
+    print(in1*in2)
+elif op == "/":
+    print(in1/in2)
+elif op == "%":
+    print(in1%in2)
+else :
+    print("UnAppropriate Operator")
