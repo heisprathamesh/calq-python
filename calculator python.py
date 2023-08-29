46c6fbf8f1d4e4e6af5f27b019a0849b920d6df7
Prathamesh karve <prathameshkarve19@gmail.com>
2023-08-29 19:13:00 +0530

    Update calculator python.py

--- calculator python.py
+++ calculator python.py
@@ -17,3 +17,60 @@ else :
     print("UnAppropriate Operator")
     print("you are supposed to give the first number in  the first input box and then operater in second box and then finally the second number)
 
+in1 = int(input("Enter First Number "))
+op= input("Enter Your Operator")
+in2= int(input("Enter Your Sec Number"))
+#python python python
+if op == "+":
+    print(in1 + in2)
+
+elif op == "-":
+    print(in1 + in2)
+elif op == "*":
+    print(in1*in2)
+elif op == "/":
+    print(in1/in2)
+elif op == "%":
+    print(in1%in2)
+else :
+    print("UnAppropriate Operator")
+    print("you are supposed to give the first number in  the first input box and then operater in second box and then finally the second number)
+
+in1 = int(input("Enter First Number "))
+op= input("Enter Your Operator")
+in2= int(input("Enter Your Sec Number"))
+#python python python
+if op == "+":
+    print(in1 + in2)
+
+elif op == "-":
+    print(in1 + in2)
+elif op == "*":
+    print(in1*in2)
+elif op == "/":
+    print(in1/in2)
+elif op == "%":
+    print(in1%in2)
+else :
+    print("UnAppropriate Operator")
+    print("you are supposed to give the first number in  the first input box and then operater in second box and then finally the second number)
+
+in1 = int(input("Enter First Number "))
+op= input("Enter Your Operator")
+in2= int(input("Enter Your Sec Number"))
+#python python python
+if op == "+":
+    print(in1 + in2)
+
+elif op == "-":
+    print(in1 + in2)
+elif op == "*":
+    print(in1*in2)
+elif op == "/":
+    print(in1/in2)
+elif op == "%":
+    print(in1%in2)
+else :
+    print("UnAppropriate Operator")
+    print("you are supposed to give the first number in  the first input box and then operater in second box and then finally the second number)
+
